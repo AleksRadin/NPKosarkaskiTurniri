e9314f6bb5ceb9e2794566ab38ed19382482b88c
aleksandar-radin <ar20190328@student.fon.bg.ac.rs>
2024-09-04 21:36:44 +0200

    Revert "Added JSON functionality for connection with database"
    This reverts commit 623f5383902b41ca82e0df296eee5476128bc30c.

--- Server/src/main/java/repository/db/DbConnection.java
+++ Server/src/main/java/repository/db/DbConnection.java
@@ -1,15 +1,11 @@
 package repository.db;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
-
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 
 
 
@@ -33,22 +29,13 @@ public class DbConnection {
 
     public Connection getConnection() throws SQLException, FileNotFoundException, IOException {
         if(connection == null || connection.isClosed()){
-            try(FileReader fr = new FileReader("src/main/java/resources/dbconfig.json")){
-				Gson gson = new Gson();
-				JsonObject jsonObject = gson.fromJson(fr, JsonObject.class);
-				
-				 String url = jsonObject.get("url").getAsString();
-	             String username = jsonObject.get("username").getAsString();
-	             String password = jsonObject.get("password").getAsString();
-	             
-	             connection = DriverManager.getConnection(url, username, password);
-	             connection.setAutoCommit(false);
-	             
-			} catch (Exception e) {
-				e.printStackTrace();
-                throw e;
-			}
-            
+            Properties properties = new Properties();
+            properties.load(new FileInputStream("config/dbconfig.properties"));
+            String url = properties.getProperty("url");
+            String username = properties.getProperty("username");
+            String password = properties.getProperty("password");
+            connection = DriverManager.getConnection(url, username, password);
+            connection.setAutoCommit(false);
         }
         return connection;
     }
